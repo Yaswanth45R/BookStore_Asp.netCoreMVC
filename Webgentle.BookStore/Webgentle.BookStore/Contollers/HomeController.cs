d355f250820515e610b1650d8b7055a2501645f5
Yaswanth <pyaswanth4@gmail.com>
2023-02-21 10:31:55 +0530

    Covert application to MVC by adding a service

---
+++ Webgentle.BookStore/Webgentle.BookStore/Contollers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webgentle.BookStore.Contollers
+{
+    public class HomeController:Controller
+    {
+        public string Index()
+        {
+            return "Hello from WebGentle";
+        }
+    }
+}
